4835d7ac337a47a6487244544fbd2d9b436ba997
Mishanya24 <mikem15@mail.ru>
2021-10-30 13:05:29 +0300

    [1] сделал работу в одном файле и разобрался куда его прикреплять)))

---
+++ ИС-31Б/Маранин/laba 30.10.py
@@ -0,0 +1,82 @@
+# 1
+
+
+import math
+
+dict={'М':math.log(14),'А': math.log(1),'Р': math.log(18),'А':math.log(1),'Н':math.log(15),'И':math.log(10),'Н':math.log(15)}
+
+print(dict['М'])
+print(dict['А'])
+print(dict['Р'])
+print(dict['А'])
+print(dict['Н'])
+print(dict['И'])
+print(dict['Н'])
+
+#2
+
+
+import math
+
+d={'М':math.log(14),'А': math.log(1),'Р': math.log(18),'А':math.log(1),'Н':math.log(15),'И':math.log(10),'Н':math.log(15)}
+
+d.items()
+
+sorted_tuple = sorted(d.items(), key=lambda x: x[0])
+
+dict(sorted_tuple)
+
+print(sorted_tuple)
+
+
+#3
+
+
+import math
+
+d={'М':math.log(14),'А': math.log(1),'Р': math.log(18),'А':math.log(1),'Н':math.log(15),'И':math.log(10),'Н':math.log(15)}
+
+sorted_tuple = sorted(d.items(), key=lambda x: x[1])
+
+dict(sorted_tuple)
+
+print(sorted_tuple)
+
+#4
+
+import math
+
+d={'М': 'math.log(14)','А': 'math.log(1)','Р': 'math.log(18)','А':'math.log(1)','Н':'math.log(15)','И':'math.log(10)','Н':'math.log(15)'}
+
+valuesList=list(d.values())
+
+generator=[x for x in valuesList if x!='math.log(14)']
+
+print(*[x for x in valuesList if x!='math.log(14)'])
+
+#5
+
+import math
+
+d={'М': math.log(14),'А': math.log(1),'Р': math.log(18),'А':math.log(1),'Н':math.log(15),'И':math.log(10),'Н':math.log(15)}
+
+valuesList=list(d.values())
+
+sr_znach=sum(valuesList)/len(valuesList)
+
+menshe=[]
+bolshe=[]
+for x in valuesList:
+    if x <= sr_znach:
+        menshe.append(x)
+    else:
+        bolshe.append(x)
+
+print(bolshe)
+print(menshe)
+        
+        
+
+
+
+
