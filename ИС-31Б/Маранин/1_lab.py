b3e13f787f554c41817bdc122ebbdcdf1ee67a96
Mishanya24 <mikem15@mail.ru>
2021-10-30 12:33:27 +0300

    [1] сделал в одном файле

--- ИС-31Б/Маранин/1_lab.py
+++ ИС-31Б/Маранин/1_lab.py
@@ -1,13 +1,85 @@
-mixed_list=[5,.2424,'misja',(5+9j),'124',56432,-24.24142,None]
+# 1
 
 
-filtered_list=[x for x in mixed_list if x!= None]
+import math
 
-tuple_list=(5,2,.24,'aboba',-23424.24,None,None)
+dict={'М':math.log(14),'А': math.log(1),'Р': math.log(18),'А':math.log(1),'Н':math.log(15),'И':math.log(10),'Н':math.log(15)}
 
-dict_list={'int': 16768, 'float': -24.232, 'str': 'soda_luv', 'complex':(5+1.3j),'ept':None}
+print(dict['М'])
+print(dict['А'])
+print(dict['Р'])
+print(dict['А'])
+print(dict['Н'])
+print(dict['И'])
+print(dict['Н'])
 
-filt_dict={key:value for key, value in dict_list.items() if len(key)>3 or value!=None}
+#2
 
-print(filt_dict)
-print(filterd_list)
+
+import math
+
+d={'М':math.log(14),'А': math.log(1),'Р': math.log(18),'А':math.log(1),'Н':math.log(15),'И':math.log(10),'Н':math.log(15)}
+
+d.items()
+
+sorted_tuple = sorted(d.items(), key=lambda x: x[0])
+
+dict(sorted_tuple)
+
+print(sorted_tuple)
+
+
+#3
+
+
+import math
+
+d={'М':math.log(14),'А': math.log(1),'Р': math.log(18),'А':math.log(1),'Н':math.log(15),'И':math.log(10),'Н':math.log(15)}
+
+sorted_tuple = sorted(d.items(), key=lambda x: x[1])
+
+dict(sorted_tuple)
+
+print(sorted_tuple)
+
+#4
+
+import math
+
+d={'М': 'math.log(14)','А': 'math.log(1)','Р': 'math.log(18)','А':'math.log(1)','Н':'math.log(15)','И':'math.log(10)','Н':'math.log(15)'}
+
+valuesList=list(d.values())
+
+generator=[x for x in valuesList if x!='math.log(14)']
+
+print(*[x for x in valuesList if x!='math.log(14)'])
+
+#5
+
+import math
+
+d={'М': math.log(14),'А': math.log(1),'Р': math.log(18),'А':math.log(1),'Н':math.log(15),'И':math.log(10),'Н':math.log(15)}
+
+valuesList=list(d.values())
+
+sr_znach=sum(valuesList)/len(valuesList)
+
+menshe=[]
+bolshe=[]
+for x in valuesList:
+    if x <= sr_znach:
+        menshe.append(x)
+    else:
+        bolshe.append(x)
+
+print(bolshe)
+print(menshe)
+        
+        
+
+
+
+
+
+
+        
