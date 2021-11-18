2c6b384298e368d2a2def068cc48a0b99b751b6c
DenosScarlet <dienis.davydov.2003@mail.ru>
2021-11-18 15:49:58 +0300

    [1] Работа со списками, словарями и генераторами
    Добавил 4 задание (Не совсем понял, что требуется от генератора. Сначала пытался создать словарь с помощью генератора с нуля, не получилось.  Использовал готовый словарь с ключами и значениями и с помощью генератора поменял значения на факториалы от этих значений.)

--- ИС-32/Давыдов Денис Николаевич/1_lab.py
+++ ИС-32/Давыдов Денис Николаевич/1_lab.py
@@ -9,6 +9,7 @@ def f(n):
 dict = {'D': f(4), 'A': f(1), 'V': f(22), 'Y': f(25), 'O': f(15)}
 print(dict)
 #Первое задание
+
 sorted_dict = sorted(dict.items())
 
 print(sorted_dict)
@@ -26,3 +27,10 @@ file_to_save = open('Third.txt', 'w+')
 file_to_save.write(str(sorted_by_value_dict))
 file_to_save.close()
 #Третье задание
+
+dict_for_gen = {'D': 4, 'A': 1, 'V': 22, 'Y': 25, 'O': 15}
+
+fun_gen = {key: f(int(value)) for key, value in dict_for_gen.items()}
+
+print(fun_gen)
+#Четвёртое задание
--- ИС-32/Давыдов Денис Николаевич/test.py
+++ ИС-32/Давыдов Денис Николаевич/test.py
@@ -1,28 +1,13 @@
-import operator
-
-
 def f(n):
     if n == 0:
         return 1
-    return f(n-1) * n
+    return f(n - 1) * n
 
-dict = {'D': f(4), 'A': f(1), 'V': f(22), 'Y': f(25), 'O': f(15)}
-print(dict)
-#Первое задание
-sorted_by_key_dict = sorted(dict.items())
 
-print(sorted_by_key_dict)
+dict = {'D': 4, 'A': 1, 'V': 22, 'Y': 25, 'O': 15}
 
-file_to_save = open('Second.txt', 'w+')
-file_to_save.write(str(sorted_by_key_dict))
-file_to_save.close()
-#Второе задание
+data = ()
 
-sorted_by_value_dict = sorted(dict.items(), key=operator.itemgetter(1), reverse=True)
+fun_gen = {key: f(int(value)) for key, value in dict.items()}
 
-print(sorted_by_value_dict)
-
-file_to_save = open('Third.txt', 'w+')
-file_to_save.write(str(sorted_by_value_dict))
-file_to_save.close()
-#Третье задание
+print(fun_gen)
