07de5c74395667cd03f17fa39c69de065824b72c
evgeny2117 <evgeny2117@yandex.ru>
2021-11-20 09:57:37 +0300

    [1] Лабораторная работа

---
+++ ИС-32/Деднев Евгений Дмитриевич/[1].py
@@ -0,0 +1,28 @@
+dictionary={'d': 4**3, 'e': 5**3, 'd': 4**3, 'n': 14**3, 'e': 5**3, 'v': 22**3}
+dictionary.items()
+sorted_increase = sorted(dictionary.items(), key=lambda x: x[0])
+dict(sorted_increase)
+sorted_waning = sorted(dictionary.items(), key=lambda x: x[0], reverse=True)
+dict(sorted_waning)
+print(sorted_increase)
+print(sorted_waning)
+for key in dictionary:
+    x = dictionary[key]
+    y= round(pow(x,1/3),10)
+    print (x,y)
+    if y%1==0:
+        print('yes')
+    else:
+        print('no')
+list1 = list(dictionary.values())
+listbiger=[]
+listsmaller=[]
+list1 = list(dictionary.values())
+srlist=sum(dictionary.values())/len(dictionary)
+for x in list1:
+    if x>=srlist:
+        listbiger.append(x)
+    else:
+        listsmaller.append(x)
+print(listbiger)
+print(listsmaller)
